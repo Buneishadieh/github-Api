b617948dd547414156ca12198af4a08b13a654b1
Bunei shadieh <buneishadrack5@gmail.com>
2021-02-06 13:52:36 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { RoutingModule } from './routing/routing.module'
-import { FormsModule } from '@angular/forms'
-// Defining routes
-// const routes:Routes=[
-//   {path:"users",component:UsersComponent},
-//   {path:"repos",component:ReposComponent},
-//   {path:"",redirectTo:"/users",pathMatch:"full"},
-//   {path:'**',component:NotFoundComponent}
-// ]
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { ReposComponent } from './repos/repos.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { DataService } from './services/data.service';
-import { HttpModule } from '@angular/http';
-import { FontDirective } from './font-directive/font.directive';
-import { LastPipe } from './pipe/last.pipe';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { NgxNavigationWithDataComponent } from "ngx-navigation-with-data";
+import "angular2-navigate-with-data";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MyProfileComponent } from "./my-profile/my-profile.component";
+
+import { MyProfileService } from "./my-profile/my-profile.service";
+import { UsersModule } from "./users/users.module";
+import { ReposModule } from "./repo/repos.module";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UnderlineDirective } from './underline.directive';
+import { SortPipe } from './sort.pipe';
+import { RoutingModule } from 'angular-routing';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersComponent,
-    ReposComponent,
-    NotFoundComponent,
-    FontDirective,
-    LastPipe
-  ],
+  declarations: [AppComponent, MyProfileComponent, PageNotFoundComponent, NavBarComponent, UnderlineDirective, SortPipe],
   imports: [
     BrowserModule,
-    RoutingModule,
-    HttpModule,
-    FormsModule
-
+    HttpClientModule,
+    UsersModule,
+    ReposModule,
+    AppRoutingModule,
+    RoutingModule.forRoot()
   ],
-  providers: [DataService],
+  providers: [MyProfileService, NgxNavigationWithDataComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
